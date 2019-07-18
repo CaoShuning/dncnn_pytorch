c4694c607b46a4d1245dd1b442be11cbaf85c648
caoshuning <sn_cao@hust.edu.cn>
2019-07-18 11:14:24 +0800

    weight_decay = 1e-4

--- TrainingCodes/dncnn_pytorch/main_test.py
+++ TrainingCodes/dncnn_pytorch/main_test.py
@@ -31,13 +31,13 @@ from skimage.io import imread, imsave
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--set_dir', default='data/Test', type=str, help='directory of test dataset')
+    parser.add_argument('--set_dir', default='data', type=str, help='directory of test dataset')
     parser.add_argument('--set_names', default=['Set68', 'Set12'], help='directory of test dataset')
     parser.add_argument('--sigma', default=25, type=int, help='noise level')
     parser.add_argument('--model_dir', default=os.path.join('models', 'DnCNN_sigma25'), help='directory of the model')
-    parser.add_argument('--model_name', default='model_001.pth', type=str, help='the model name')
+    parser.add_argument('--model_name', default='model_180.pth', type=str, help='the model name')
     parser.add_argument('--result_dir', default='results', type=str, help='directory of test dataset')
-    parser.add_argument('--save_result', default=0, type=int, help='save the denoised image, 1 or 0')
+    parser.add_argument('--save_result', default=1, type=int, help='save the denoised image, 1 or 0')
     return parser.parse_args()
 
 
@@ -163,7 +163,8 @@ if __name__ == '__main__':
                 ssim_x_ = compare_ssim(x, x_)
                 if args.save_result:
                     name, ext = os.path.splitext(im)
-                    show(np.hstack((y, x_)))  # show the image
+                    # show(np.hstack((y, x_)))  # show the image
+                    save_result(y, path=os.path.join(args.result_dir, set_cur, name + '_dncnn_noisy' + ext))   # save the noisy image
                     save_result(x_, path=os.path.join(args.result_dir, set_cur, name+'_dncnn'+ext))  # save the denoised image
                 psnrs.append(psnr_x_)
                 ssims.append(ssim_x_)
